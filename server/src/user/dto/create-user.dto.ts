3f0bd119347539d79d98a55a05270c5f6dee8aed
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-01-24 15:39:39 +0300

    Pipes. Валидация входных данных. Class-validator. @IsString, @IsEmail, @Length

--- server/src/user/dto/create-user.dto.ts
+++ server/src/user/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString({ message: 'Должно быть строкой' })
+  @IsEmail({}, { message: 'Некорректный email' })
   readonly email: string;
+
+  @IsString({ message: 'Должно быть строкой' })
+  @Length(4, 16, { message: 'Пароль не должен быть меньше 4 и больше 16 символов' })
   readonly password: string;
 }
--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,13 +1,15 @@
+import { ValidationPipe } from './../pipes/validation.pipe';
 import { RoleGuard } from './../auth/role.guard';
 import { Role } from './../auth/role-auth.decorator';
 import { UserService } from './user.service';
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, UsePipes } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @UsePipes(ValidationPipe)
   @Post()
   create(@Body() dto: CreateUserDto) {
     return this.userService.createUser(dto);
