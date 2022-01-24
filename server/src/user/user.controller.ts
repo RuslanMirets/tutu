149eae4dd96dc596f0d3ae6834cf70a9bbcfa9c5
Ruslan Mirets <ruslan.mirets@gmail.com>
2022-01-24 11:05:53 +0300

    Запись\Получение. Repository. Работа с БД.

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.userService.createUser(dto);
+  }
+
+  @Get()
+  getAll() {
+    return this.userService.getAllUsers();
+  }
+}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,19 @@
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserModel } from './user.model';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(UserModel) private userRepository: typeof UserModel) {}
+
+  async createUser(dto: CreateUserDto) {
+    const user = await this.userRepository.create(dto);
+    return user;
+  }
+
+  async getAllUsers() {
+    const users = await this.userRepository.findAll();
+    return users;
+  }
+}
